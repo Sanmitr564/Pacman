a964f9c7f3dc17ce329ee44d2f57d15e7d03adcb
Sanmitr564 <generic39542@gmail.com>
2022-05-20 11:58:22 -0700

    separate move method

--- src/Ghost.java
+++ src/Ghost.java
@@ -30,17 +30,12 @@ public class Ghost {
         return y;
     }
 
-    public void move(Tile[][] board, Player player) {
-        if (section == numSections / 2) {
-            if (board[y][x] == Tile.JUNCTION) {
-                direction = getDirectionAtJunction(board, player);
-            } else if (board[y][x] == Tile.SPECIAL_JUNCTION) {
-                direction = getDirectionAtSpecialJunction(board, player);
-            } else if (tryGetTile(board) == null || tryGetTile(board) == Tile.WALL) {
-                direction = getValidDirections(board)[0];
-            }
-        }
+    public void update(Tile[][] board, Player player) {
+        updateDirection(board, player.getY(), player.getX());
+        move();
+    }
 
+    protected void move() {
         switch (direction) {
             case UP, RIGHT -> section++;
             case LEFT, DOWN -> section--;
@@ -54,6 +49,40 @@ public class Ghost {
                 case DOWN -> y--;
             }
             section = section < 0 ? section + numSections : section % numSections;
+        }
+    }
+
+    protected void updateDirection(Tile[][] board, Player player) {
+        if (section == numSections / 2) {
+            if (board[y][x] == Tile.TELEPORT) {
+                switch (x) {
+                    case 0 -> x = 27;
+                    case 27 -> x = 0;
+                }
+            } else if (board[y][x] == Tile.JUNCTION) {
+                direction = getDirectionAtJunction(board, player);
+            } else if (board[y][x] == Tile.SPECIAL_JUNCTION) {
+                direction = getDirectionAtSpecialJunction(board, player);
+            } else if (tryGetTile(board) == null || tryGetTile(board) == Tile.WALL) {
+                direction = getValidDirections(board)[0];
+            }
+        }
+    }
+
+    protected void updateDirection(Tile[][] board, int targetRow, int targetCol){
+        if (section == numSections / 2) {
+            if (board[y][x] == Tile.TELEPORT) {
+                switch (x) {
+                    case 0 -> x = 27;
+                    case 27 -> x = 0;
+                }
+            } else if (board[y][x] == Tile.JUNCTION) {
+                direction = getDirectionAtJunction(board, targetRow, targetCol);
+            } else if (board[y][x] == Tile.SPECIAL_JUNCTION) {
+                direction = getDirectionAtSpecialJunction(board, targetRow, targetCol);
+            } else if (tryGetTile(board) == null || tryGetTile(board) == Tile.WALL) {
+                direction = getValidDirections(board)[0];
+            }
         }
     }
 
@@ -102,6 +131,17 @@ public class Ghost {
         return d;
     }
 
+    protected Direction getDirectionAtJunction(Tile[][] board, int targetRow, int targetCol) {
+        Direction[] validDirections = getValidDirections(board);
+        Direction d = validDirections[0];
+        for (int i = 1; i < validDirections.length; i++) {
+            if (getDistance(d, targetRow, targetCol) < getDistance(validDirections[i], targetRow, targetCol)) {
+                d = validDirections[i];
+            }
+        }
+        return d;
+    }
+
     protected Direction getDirectionAtSpecialJunction(Tile[][] board, Player player) {
         Direction[] validDirections = getValidSpecialDirections(board);
         Direction d = validDirections[0];
@@ -113,10 +153,24 @@ public class Ghost {
         return d;
     }
 
-    private Direction[] getValidSpecialDirections(Tile[][] board) {
+    protected Direction getDirectionAtSpecialJunction(Tile[][] board, int targetRow, int targetCol) {
+        Direction[] validDirections = getValidSpecialDirections(board);
+        Direction d = validDirections[0];
+        for (int i = 1; i < validDirections.length; i++) {
+            if (getDistance(d, targetRow, targetCol) < getDistance(validDirections[i], targetRow, targetCol)) {
+                d = validDirections[i];
+            }
+        }
+        return d;
+    }
+
+    protected Direction[] getValidSpecialDirections(Tile[][] board) {
         List<Direction> directions = new ArrayList<>();
         for (int i = 0; i < Direction.values().length; i++) {
-            if (Direction.values()[i] == DirectionHelp.getOppositeDirection(direction) || Direction.values()[i] == Direction.UP) {
+            if (
+                    Direction.values()[i] == DirectionHelp.getOppositeDirection(direction) ||
+                            Direction.values()[i] == Direction.UP
+            ) {
                 continue;
             }
             Tile t = tryGetTile(board, Direction.values()[i]);
@@ -129,9 +183,14 @@ public class Ghost {
         return d;
     }
 
-    private int getDistance(Direction d, Player player) {
+    protected int getDistance(Direction d, Player player) {
         int[] offsets = DirectionHelp.getOffsets(d);
         return ((player.getX() - x + offsets[1]) * (player.getX() - x + offsets[1])) + (player.getY() - y + offsets[0]) * (player.getY() - y + offsets[0]);
+    }
+
+    protected int getDistance(Direction d, int targetRow, int targetCol) {
+        int[] offsets = DirectionHelp.getOffsets(d);
+        return ((targetCol - x + offsets[1]) * (targetCol - x + offsets[1])) + (targetRow - y + offsets[0]) * (targetRow - y + offsets[0]);
     }
 
     public float[] getTileOffsets() {//{xOffset, yOffset}
