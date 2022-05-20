96988e18367b4d17f29621574849fdf995b49747
Sanmitr564 <generic39542@gmail.com>
2022-05-20 11:59:48 -0700

    update constructor

--- src/Blinky.java
+++ src/Blinky.java
@@ -1,6 +1,6 @@
 public class Blinky extends Ghost{
 
-    public Blinky(){
-        super(26,29, Direction.LEFT, 25, 50);
+    public Blinky(int x, int y, Direction direction, int section, int numSections){
+        super(x, y, direction, section, numSections);
     }
 }
