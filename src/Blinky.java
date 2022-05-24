4f4b4fe80b2084feb001f4705b4cc15fb7c909b4
Sanmitr564 <generic39542@gmail.com>
2022-05-24 08:32:33 -0700

    Add release

--- src/Blinky.java
+++ src/Blinky.java
@@ -1,6 +1,6 @@
 public class Blinky extends Ghost{
 
-    public Blinky(int x, int y, Direction direction, int section, int numSections){
-        super(x, y, direction, section, numSections);
+    public Blinky(int x, int y, Direction direction, int section, int numSections, boolean released){
+        super(x, y, direction, section, numSections, released);
     }
 }
