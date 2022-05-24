7bca385ef95523fe0fb3678738606ab56c108a49
Sanmitr564 <generic39542@gmail.com>
2022-05-24 08:31:35 -0700

    Use playerboard

--- src/PacMan.java
+++ src/PacMan.java
@@ -11,16 +11,19 @@ public class PacMan {
 
     private Player player;
 
+    private Tile[][] ghostBoard;
     private Tile[][] playerBoard;
 
     public PacMan() throws FileNotFoundException {
         player = new Player();
+        ghostBoard = new Tile[Global.BOARD_ROWS][Global.BOARD_COLS];
         playerBoard = new Tile[Global.BOARD_ROWS][Global.BOARD_COLS];
-        initializeBoard();
-        blinky = new Blinky(1, 1, Direction.RIGHT, 25, 25);
-        inky = new Inky(1, 27, Direction.DOWN, 25, 25);
-        pinky = new Pinky(20, 1, Direction.LEFT, 25, 25);
-        clyde = new Clyde(1, 1, Direction.RIGHT, 25, 25);
+        initializeGhostBoard();
+        initializePlayerBoard();
+        blinky = new Blinky(1, 1, Direction.RIGHT, 25, 25, true);
+        inky = new Inky(1, 27, Direction.DOWN, 25, 25, false);
+        pinky = new Pinky(20, 1, Direction.LEFT, 25, 25, false);
+        clyde = new Clyde(1, 1, Direction.RIGHT, 25, 25, false);
     }
 
     public Player getPlayer() {
@@ -43,12 +46,31 @@ public class PacMan {
         return clyde;
     }
 
+    public Tile[][] getGhostBoard() {
+        return ghostBoard;
+    }
+
     public Tile[][] getPlayerBoard() {
         return playerBoard;
     }
 
-    private void initializeBoard() throws FileNotFoundException {
+    private void initializeGhostBoard() throws FileNotFoundException {
         File file = new File("C:/Users/Sanmitr/Documents/IdeaProjects/Pacman/src/ghostMap.txt");
+        Scanner scanner = new Scanner(file);
+        int row = Global.BOARD_ROWS - 1;
+        while (scanner.hasNextLine()) {
+            String str = scanner.nextLine();
+            for (int col = 0; col < str.length(); col++) {
+                char value = str.charAt(col);
+                int num = Integer.parseInt(String.valueOf(value));
+                ghostBoard[row][col] = Tile.values()[num];
+            }
+            row--;
+        }
+    }
+
+    private void initializePlayerBoard() throws FileNotFoundException {
+        File file = new File("C:/Users/Sanmitr/Documents/IdeaProjects/Pacman/src/playerMap.txt");
         Scanner scanner = new Scanner(file);
         int row = Global.BOARD_ROWS - 1;
         while (scanner.hasNextLine()) {
@@ -64,10 +86,13 @@ public class PacMan {
 
     public void update() {
         player.move(playerBoard);
-        blinky.update(playerBoard, player);
-        inky.update(playerBoard, player, blinky);
-        pinky.update(playerBoard, player);
-        clyde.update(playerBoard, player, blinky);
+        if(player.getConsumedSmallPellets() > 30 && !inky.isReleased()){
+            inky.release();
+        }
+        blinky.update(ghostBoard, player);
+        inky.update(ghostBoard, player, blinky);
+        pinky.update(ghostBoard, player);
+        clyde.update(ghostBoard, player, blinky);
     }
 
 }
