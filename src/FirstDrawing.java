ff846da3d3ff4f17ddd40d40dfdaf99f59940b68
Sanmitr564 <generic39542@gmail.com>
2022-06-05 20:51:13 -0700

    Render menu

--- src/FirstDrawing.java
+++ src/FirstDrawing.java
@@ -5,6 +5,8 @@ import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.viewport.*;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -23,6 +25,8 @@ public class FirstDrawing extends ApplicationAdapter implements InputProcessor {
 
     private PacMan pacman;
 
+    private GameState gameState;
+
     @Override//called once when we start the game
     public void create() {
 
@@ -33,6 +37,7 @@ public class FirstDrawing extends ApplicationAdapter implements InputProcessor {
         batch = new SpriteBatch();//if you want to use images instead of using ShapeRenderer
 
         pacman = new PacMan();
+        gameState = GameState.MENU;
 
         Gdx.input.setInputProcessor(this);
     }
@@ -40,10 +45,14 @@ public class FirstDrawing extends ApplicationAdapter implements InputProcessor {
     @Override//called 60 times a second
     public void render() {
         preRender();
-        renderBoard();
-        renderPlayer();
-        renderGhost();
-        pacman.update();
+        if (gameState == GameState.MENU) {
+            renderMenu();
+        } else if (gameState == GameState.PACMAN) {
+            renderBoard();
+            renderPlayer();
+            renderGhost();
+            pacman.update();
+        }
     }
 
     @Override
@@ -73,7 +82,7 @@ public class FirstDrawing extends ApplicationAdapter implements InputProcessor {
         for (int row = 0; row < Global.BOARD_ROWS; row++) {
             for (int col = 0; col < Global.BOARD_COLS; col++) {
 
-                switch(pacman.getPlayerBoard()[row][col]){
+                switch (pacman.getPlayerBoard()[row][col]) {
                     case WALL -> renderer.setColor(Color.BLUE);
                     case STRAIGHT, GHOST_AREA, TELEPORT -> renderer.setColor(Color.BLACK);
                     case SMALL_PELLET -> renderer.setColor(Color.OLIVE);
@@ -89,6 +98,9 @@ public class FirstDrawing extends ApplicationAdapter implements InputProcessor {
     private void renderPlayer() {
         renderer.begin(ShapeRenderer.ShapeType.Filled);
         renderer.setColor(Color.YELLOW);
+        if (pacman.getPlayer().isEnergized()) {
+            renderer.setColor(Color.GREEN);
+        }
         //Gdx.gl.glLineWidth(3f);
         float[] tileOffsets = pacman.getPlayer().getTileOffsets();
         renderer.circle(
@@ -102,6 +114,9 @@ public class FirstDrawing extends ApplicationAdapter implements InputProcessor {
     private void renderGhost() {
         renderer.begin(ShapeRenderer.ShapeType.Filled);
         renderer.setColor(Color.RED);
+        if(pacman.getPlayer().isEnergized()){
+            renderer.setColor(Color.BLUE);
+        }
         //Gdx.gl.glLineWidth(3f);
         float[] tileOffsets = pacman.getBlinky().getTileOffsets();
         renderer.circle(
@@ -110,6 +125,9 @@ public class FirstDrawing extends ApplicationAdapter implements InputProcessor {
                 Global.TILE_SIZE / 2f
         );
         renderer.setColor(Color.CYAN);
+        if(pacman.getPlayer().isEnergized()){
+            renderer.setColor(Color.BLUE);
+        }
         tileOffsets = pacman.getInky().getTileOffsets();
         renderer.circle(
                 pacman.getInky().getX() * Global.TILE_SIZE + Global.FIELD_X + tileOffsets[0],
@@ -117,6 +135,9 @@ public class FirstDrawing extends ApplicationAdapter implements InputProcessor {
                 Global.TILE_SIZE / 2f
         );
         renderer.setColor(Color.PINK);
+        if(pacman.getPlayer().isEnergized()){
+            renderer.setColor(Color.BLUE);
+        }
         tileOffsets = pacman.getPinky().getTileOffsets();
         renderer.circle(
                 pacman.getPinky().getX() * Global.TILE_SIZE + Global.FIELD_X + tileOffsets[0],
@@ -124,6 +145,9 @@ public class FirstDrawing extends ApplicationAdapter implements InputProcessor {
                 Global.TILE_SIZE / 2f
         );
         renderer.setColor(Color.ORANGE);
+        if(pacman.getPlayer().isEnergized()){
+            renderer.setColor(Color.BLUE);
+        }
         tileOffsets = pacman.getClyde().getTileOffsets();
         renderer.circle(
                 pacman.getClyde().getX() * Global.TILE_SIZE + Global.FIELD_X + tileOffsets[0],
@@ -133,15 +157,94 @@ public class FirstDrawing extends ApplicationAdapter implements InputProcessor {
         renderer.end();
     }
 
+    private void renderMenu() {
+        renderPacmanIcon();
+        renderTetrisIcon();
+    }
+
+    private void renderPacmanIcon() {
+        batch.begin();
+        int mouseX = Gdx.input.getX();
+        int mouseY = Gdx.input.getY();
+        Vector2 mousePos = new Vector2(mouseX, mouseY);
+        mousePos = viewport.unproject(mousePos);
+        if (Global.PACMAN_ICON_RECTANGLE.contains(mousePos)) {
+            batch.setColor(Color.WHITE);
+        } else {
+            batch.setColor(Color.GRAY);
+        }
+        batch.draw(Global.PACMAN_ICON_REGION,
+                (Global.WORLD_WIDTH - Global.PACMAN_ICON_REGION.getRegionWidth()) / 2f,
+                (Global.WORLD_HEIGHT - Global.PACMAN_ICON_REGION.getRegionHeight()) / 2f + 150,
+                Global.PACMAN_ICON_REGION.getRegionWidth() / 2f,
+                Global.PACMAN_ICON_REGION.getRegionHeight() / 2f,
+                Global.PACMAN_ICON_REGION.getRegionWidth(),
+                Global.PACMAN_ICON_REGION.getRegionHeight(),
+                1 / Global.PACMAN_ICON_SCALE,
+                1 / Global.PACMAN_ICON_SCALE,
+                0
+        );
+        batch.end();
+    }
+
+    private void renderTetrisIcon() {
+        batch.begin();
+        int mouseX = Gdx.input.getX();
+        int mouseY = Gdx.input.getY();
+        Vector2 mousePos = new Vector2(mouseX, mouseY);
+        mousePos = viewport.unproject(mousePos);
+        if (Global.TETRIS_ICON_RECTANGLE.contains(mousePos)) {
+            batch.setColor(Color.WHITE);
+        } else {
+            batch.setColor(Color.GRAY);
+        }
+        batch.draw(Global.TETRIS_ICON_REGION,
+                (Global.WORLD_WIDTH - Global.TETRIS_ICON_REGION.getRegionWidth()) / 2f,
+                (Global.WORLD_HEIGHT - Global.TETRIS_ICON_REGION.getRegionHeight()) / 2f - 150,
+                Global.TETRIS_ICON_REGION.getRegionWidth() / 2f,
+                Global.TETRIS_ICON_REGION.getRegionHeight() / 2f,
+                Global.TETRIS_ICON_REGION.getRegionWidth(),
+                Global.TETRIS_ICON_REGION.getRegionHeight(),
+                1 / Global.TETRIS_ICON_SCALE,
+                1 / Global.TETRIS_ICON_SCALE,
+                0
+        );
+        batch.end();
+    }
+
     @Override
     public boolean keyDown(int i) {
-        switch(i){
-            case Input.Keys.UP -> pacman.getPlayer().setQueuedDirection(Direction.UP);
-            case Input.Keys.LEFT -> pacman.getPlayer().setQueuedDirection(Direction.LEFT);
-            case Input.Keys.RIGHT -> pacman.getPlayer().setQueuedDirection(Direction.RIGHT);
-            case Input.Keys.DOWN -> pacman.getPlayer().setQueuedDirection(Direction.DOWN);
+        if (gameState == GameState.MENU) {
+
+        } else if (gameState == GameState.PACMAN) {
+
+            switch (i) {
+                case Input.Keys.UP -> pacman.getPlayer().setQueuedDirection(Direction.UP);
+                case Input.Keys.LEFT -> pacman.getPlayer().setQueuedDirection(Direction.LEFT);
+                case Input.Keys.RIGHT -> pacman.getPlayer().setQueuedDirection(Direction.RIGHT);
+                case Input.Keys.DOWN -> pacman.getPlayer().setQueuedDirection(Direction.DOWN);
+                case Input.Keys.ESCAPE -> {
+                    gameState = GameState.MENU;
+                    pacman = new PacMan();
+                    return false;
+                }
+            }
+            pacman.start();
+        } else if (gameState == GameState.TETRIS) {
+
         }
-        pacman.start();
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int i, int i1, int i2, int i3) {
+        Vector2 mousePos = new Vector2(i, i1);
+        mousePos = viewport.unproject(mousePos);
+        if(Global.PACMAN_ICON_RECTANGLE.contains(mousePos)){
+            gameState = GameState.PACMAN;
+        }else if(Global.TETRIS_ICON_RECTANGLE.contains(mousePos)){
+            System.out.println("Tetris");
+        }
         return false;
     }
 
@@ -153,11 +256,6 @@ public class FirstDrawing extends ApplicationAdapter implements InputProcessor {
 
     @Override
     public boolean keyTyped(char c) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDown(int i, int i1, int i2, int i3) {
         return false;
     }
 
